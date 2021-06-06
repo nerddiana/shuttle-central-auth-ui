48f0701687cec016375c67e38cfdad5d291b3d0f
Diana Martínez <diananerdoficial@gmail.com>
2021-06-05 19:56:08 -0500

    Remove the urlencoded header from axios

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://shuttle-central-keycloak.herokuapp.com/auth',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
+  baseURL: 'https://shuttle-central-keycloak.herokuapp.com/auth'
 });
 
 export default api
